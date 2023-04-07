92bdcd9421dafd21f15549fb4dda54eda68482da
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-07 01:26:03 +0000

    Whitelist ALLOWED_HOSTS

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i=hvwa06-fy-^u)=e%z0*#6t2-7!^cwvdq8&ue_$g-x*^p!pl!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bepper-api.up.railway.app']
 
 
 # Application definition
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
-CORS_ALLOWED_ORIGINS = ['https://bepper-api.up.railway.app', 'https://bepper.cyclic.app']
+CORS_ALLOWED_ORIGINS = ['https://bepper.cyclic.app']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
